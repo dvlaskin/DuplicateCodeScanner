48b271af4eb77448b3a4e3f722f7c97109a1a335
dvlaskin <dvlaskin.mail@gmail.com>
2022-05-01 19:08:48 +0300

    init commit

---
+++ DuplicateCodeScanner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DuplicateCodeScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
